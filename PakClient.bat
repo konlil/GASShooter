d0f3a3bf4543bf76ce4e83e181bcedf00ebc0e65
yjg <konlil@163.com>
2022-12-25 11:24:00 +0800

    测试完成三种常见情况的打包

--- PakClient.bat
+++ PakClient.bat
@@ -4,10 +4,13 @@ set UE4_ENGINE_PATH=E:\Work\UE4_RG
 set PROJECT_PATH=E:\Work\GASShooter
 set PAK_OUTPUT_PATH=%PROJECT_PATH%\PakOutput
 
+rem -CookOutputDir="%PAK_OUTPUT_PATH%\CookOutput\WindowsNoEditor"   指定这个目录会报：UE4 Unreal Game files required to initialize the global shader library are missing from
+rem -archive -archivedirectory="%PAK_OUTPUT_PATH%\ArchiveOutput"  不用archive，直接使用Stage的输出（stage会自动清理输出目录）
+
 %UE4_ENGINE_PATH%\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -project="%PROJECT_PATH%\GASShooter.uproject" ^
-    -nop4 -cook -compressed -fileopenorder -iterate -CookOutputDir="%PAK_OUTPUT_PATH%\CookOutput\WindowsNoEditor" ^
+    -nop4 -cook -compressed -fileopenorder -iterate ^
     -Pak ^
     -build -platform=Win64 -clientconfig=Development -nocompileeditor ^
-    -stage -StagingDirectory="%PAK_OUTPUT_PATH%\StageOutput" ^
-    -archive -archivedirectory="%PAK_OUTPUT_PATH%\ArchiveOutput"
+    -stage -StagingDirectory="%PAK_OUTPUT_PATH%\StageOutput"
+
 pause
